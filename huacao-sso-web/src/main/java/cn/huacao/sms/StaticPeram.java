7e955a32f42e3844f2aa1ce895238a6252c7ef1f
tengyijing <42561641+tengyijing@users.noreply.github.com>
2020-05-26 12:27:50 +0800

    Update StaticPeram.java

--- huacao-sso-web/src/main/java/cn/huacao/sms/StaticPeram.java
+++ huacao-sso-web/src/main/java/cn/huacao/sms/StaticPeram.java
@@ -17,10 +17,10 @@ public class StaticPeram {
 	final static String product = "Dysmsapi";// 短信API产品名称（短信产品名固定，无需修改）
 	final static String domain = "dysmsapi.aliyuncs.com";// 短信API产品域名（接口地址固定，无需修改）
 	// 替换成你的AK (产品密)
-	final static String accessKeyId = "LTAIKTbU3Si2iDLP";// 你的accessKeyId,填你自己的 上文配置所得  自行配置
-	final static String accessKeySecret = "PT9f2ipEr4vSN1kFxyiIe4xzO9zAy4";// 你的accessKeySecret,填你自己的 上文配置所得 自行配置
+	final static String accessKeyId = "XXXX";// 你的accessKeyId,填你自己的 上文配置所得  自行配置
+	final static String accessKeySecret = "XXXXX";// 你的accessKeySecret,填你自己的 上文配置所得 自行配置
 	// 必填:短信签名-可在短信控制台中找到
-	final static String SignName = "药店网管理系统";// 阿里云配置你自己的短信签名填入
+	final static String SignName = "XXXXX";// 阿里云配置你自己的短信签名填入
 	// 必填:短信模板-可在短信控制台中找到
-	final static String TemplateCode = "SMS_163850822"; // 阿里云配置你自己的短信模板填入
+	final static String TemplateCode = "XXXXX"; // 阿里云配置你自己的短信模板填入
 }
